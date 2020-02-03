5b53cc947f8899e747ddeb599f3943b43d29d44f
dandeboer <ddeboer926@gmail.com>
2020-02-03 09:29:28 -0500

    Javascript is hard

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let nameField = document.getElementById("name-field");
 
 let form = document.querySelector("#parking-form");
 
-let isRequired = document.createElement("Div");
+//let isRequired = document.createElement("Div");
 //let text = document.createTextNode("Is Required");
 //isRequired.innerText=("Required");
 
@@ -13,20 +13,23 @@ form.addEventListener("submit", function(e) {
   e.preventDefault();
   let inputs = document.querySelectorAll(".inputClass");
   for (let inputLoop of inputs) {
-    //if (inputLoop.parentElement.classList.contains(input-invalid) = false) {}
+    //console.log(inputLoop.parentElement.)
+    //if (inputLoop.parentElement.contains(document.querySelectorAll(".invalid.color")) = false) {
     if (inputLoop.value === "") {
-        let isRequired = document.createElement("Div");
-        isRequired.innerText=("Required");
+      let isRequired = document.createElement("Div");
+      isRequired.innerText = "Required";
       inputLoop.parentElement.classList.add("input-invalid");
       inputLoop.parentElement.classList.remove("input-valid");
-      console.log(isRequired)
+      console.log(isRequired);
       inputLoop.parentElement.appendChild(isRequired);
+      isRequired.classList.add("invalid-color");
       //let isRequiredClone = isRequired.cloneNode()
     } else {
       inputLoop.parentElement.classList.add("input-valid");
       inputLoop.parentElement.classList.remove("input-invalid");
     }
-}});
+  }
+});
 
 //add a class to every imput to identify it and put those in an array and a loop
 //if input.value === ""

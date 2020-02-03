76bc8847e3d8f5e29c68a382f73a62d045bd8701
dandeboer <ddeboer926@gmail.com>
2020-02-03 15:33:34 -0500

    message

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ form.addEventListener("submit", function(e) {
   e.preventDefault();
   required = document.querySelectorAll(".invalid-color");
   for (let element of required) {
-    console.log(required);
     element.remove();
   }
   const inputs = document.querySelectorAll(".inputClass");
@@ -16,12 +15,27 @@ form.addEventListener("submit", function(e) {
       isRequired.innerText = "*Required";
       inputLoop.parentElement.classList.add("input-invalid");
       inputLoop.parentElement.classList.remove("input-valid");
-      console.log(isRequired);
       inputLoop.parentElement.appendChild(isRequired);
       isRequired.classList.add("invalid-color");
     } else {
-      inputLoop.parentElement.classList.add("input-valid");
-      inputLoop.parentElement.classList.remove("input-invalid");
+      // inputLoop.parentElement.classList.add("input-valid");
+      // inputLoop.parentElement.classList.remove("input-invalid");
+      carYearFunction()
     }
   }
 });
+// let carYear = document.querySelector("#car-year")
+
+// car year must be a number
+function carYearFunction() {
+let carYear = document.querySelector("#car-year")
+let yearCheck = Number(carYear.value)
+console.log(yearCheck)
+if (yearCheck == NaN) {
+  let needCarYear = document.createElement("div")
+  needCarYear.innerText = "Please enter a number"
+  carYear.parentElement.appendChild(needCarYear)
+  needCarYear.classList.add("invalid-color")
+} else { 
+  carYear.parentElement.classList.add("input-valid")
+}}
